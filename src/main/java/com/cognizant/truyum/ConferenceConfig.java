df68a71ac1706bd7b708d6b82a792f4ba00f41b2
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2021-03-15 15:44:10 +0530

    Changed the structure for the application

--- src/main/java/com/cognizant/truyum/ConferenceConfig.java
+++ src/main/java/com/cognizant/truyum/ConferenceConfig.java
@@ -1,12 +1,22 @@
 package com.cognizant.truyum;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class ConferenceConfig implements WebMvcConfigurer {
 
-   /* @Override
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/files/**").addResourceLocations("/WEB-INF/pdf/");
     }
@@ -38,6 +48,6 @@ public class ConferenceConfig implements WebMvcConfigurer {
         internalResourceViewResolver.setSuffix(".jsp");
         internalResourceViewResolver.setOrder(0);
         return internalResourceViewResolver;
-    }*/
+    }
 
 }
